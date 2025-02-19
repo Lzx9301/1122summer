a97002ecc9af896c53f701b3b9c92a92b8991792
Lzx9301 <nicoliu930226@gmail.com>
2025-02-19 14:34:52 +0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,22 +1,15 @@
 const students = [
-    { name: '01', account: 'jide01@infoeduorg.onmicrosoft.com', password: 'Bur38049' },
-    { name: '02', account: 'jide02@infoeduorg.onmicrosoft.com', password: 'Hac44858' },
-    { name: '03', account: 'jide03@infoeduorg.onmicrosoft.com', password: 'Mog50083' },
-    { name: '04', account: 'jide04@infoeduorg.onmicrosoft.com', password: 'Way36594' },
-    { name: '05', account: 'jide05@infoeduorg.onmicrosoft.com', password: 'Bog09023' },
-    { name: '06', account: 'jide06@infoeduorg.onmicrosoft.com', password: 'Zux21313' },
-    { name: '07', account: 'jide07@infoeduorg.onmicrosoft.com', password: 'Jor21690' },
-    { name: '08', account: 'jide08@infoeduorg.onmicrosoft.com', password: 'Kay09811' },
-    { name: '09', account: 'jide09@infoeduorg.onmicrosoft.com', password: 'Yav11244' },
-    { name: '10', account: 'jide10@infoeduorg.onmicrosoft.com', password: 'Juw48996' },
-    { name: '林羿融', account: 'chij05@infoeduorg.onmicrosoft.com', password: 'Bay72811' },
-    { name: '謝棠宇', account: 'chij06@infoeduorg.onmicrosoft.com', password: 'Zov20042' },
-    { name: '陳薇棻', account: 'chij07@infoeduorg.onmicrosoft.com', password: 'Zoc48053' },
-    { name: '張育傑', account: 'chij08@infoeduorg.onmicrosoft.com', password: 'Gog98265' },
-    { name: '施聖瀚', account: 'chij09@infoeduorg.onmicrosoft.com', password: 'Tuk29805' },
-    { name: '林佑誠', account: 'chij10@infoeduorg.onmicrosoft.com', password: 'Tuv02992' },
-    { name: '羅昱為', account: 'chij11@infoeduorg.onmicrosoft.com', password: 'Yay45825' },
-    { name: '施鈞彥', account: 'chij12@infoeduorg.onmicrosoft.com', password: 'Bay54371' },
+    { name: '01', account: 'infostudent01', password: 'Zaz12762' },
+    { name: '02', account: 'infostudent02', password: 'Cov37430' },
+    { name: '03', account: 'infostudent03', password: 'Yux85462' },
+    { name: '04', account: 'infostudent04', password: 'Nux55035' },
+    { name: '05', account: 'infostudent05', password: 'Fub28527' },
+    { name: '06', account: 'infostudent01', password: 'Zaz12762' },
+    { name: '07', account: 'infostudent02', password: 'Cov37430' },
+    { name: '08', account: 'infostudent03', password: 'Yux85462' },
+    { name: '09', account: 'infostudent04', password: 'Nux55035' },
+    { name: '10', account: 'infostudent05', password: 'Fub28527' },
+    { name: '11', account: 'infostudent01', password: 'Zaz12762' },
     { name: 'zx', account: 'teacher41@infoeduorg.onmicrosoft.com', password: 'Wok76804' },
     { name: 'oa', account: 'orangeapple.teacher5@orangeapple.co', password: 'OAteacher5' },
 
@@ -68,3 +61,7 @@ function Pid(){
     $("#searchButton").show()
     $("#studentInfo").show()
 }
+
+document.getElementById("bytefox").addEventListener("click", function() {
+    alert("比特狐：嗨，我來幫你學習程式！");
+});
